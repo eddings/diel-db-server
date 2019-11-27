ddb0f8b0f0ccc29f9b1ea4bae57ff394faf124d0
Lucie Choi <lucie0407@berkeley.edu>
2019-11-27 14:24:31 -0800

    before merging

--- tests/testTimeseriesSqlite.ts
+++ tests/testTimeseriesSqlite.ts
@@ -15,4 +15,21 @@ function runWithSqlite() {
   StartDielDbServer([config]);
   console.log(`We are starting a server with data found in ${path}.`);
 }
-runWithSqlite();
+
+function runWithSqliteMultiple(i: number) {
+  console.log("\x1b[43m", `Connecting to Sqlite.`, "\x1b[0m");
+  const path = resolve(`tests/data/remote_db${i}.sqlite`);
+  const config: SqliteDbConfig = {
+    dbName: `remote_db${i}`,
+    driver: DbDriver.SQLite,
+    path
+  };
+  StartDielDbServer([config]);
+  console.log(`We are starting a server with data found in ${path}.`);
+}
+const whichSqlite:number = parseInt(process.argv[2]);
+if (whichSqlite) {
+  runWithSqliteMultiple(whichSqlite);
+} else {
+  runWithSqlite();
+}
